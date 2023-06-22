8c7c5cb85d9c377293b232098594de4944a686a8
Gianluca Tiengo <gl.tiengo@gmail.com>
2023-06-22 16:36:10 +0200

    working on style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import {tns}  from 'tiny-slider/src/tiny-slider';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-
-
+export class HomeComponent implements AfterViewInit{
+  ngAfterViewInit() {
+    const slider = tns({
+      container: '#slider',
+      items: 5,
+      slideBy: 'page',
+      autoplay: true,
+      autoplayButtonOutput: false,
+      controls: false,
+      nav: false,
+    });
+  }
 }
