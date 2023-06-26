2f7cadc2430e926d6be122e3f871838c48718e07
Gianluca Tiengo <gl.tiengo@gmail.com>
2023-06-26 16:28:53 +0200

    skill up & knowdlege

--- src/app/knowledge/knowledge.component.ts
+++ src/app/knowledge/knowledge.component.ts
@@ -7,4 +7,55 @@ import { Component } from '@angular/core';
 })
 export class KnowledgeComponent {
 
+  links = [
+    {
+      id:1,
+      serial:'01',
+      icon:' fa-house-signal',
+      name: 'Public APIs',
+      desc:'Looking for one that fits your needs can be incredibly time-consuming. Here A collective list of free APIs for use in software and web development.',
+      url:'https://letudev.com/publicapis',
+    },
+    {
+      id:2,
+      serial:'02',
+      icon:'fa-pencil-paintbrush',
+      name: 'UI Graphics',
+      desc:'Websites and resources with modern UI components in different formats such as PSD, Sketch, Figma, etc. They are great for ideas for web components/UI.',
+      url:'https://letudev.com/uigraphics',
+    },
+    {
+      id:3,
+      serial:'03',
+      icon:'fa-fire',
+      name: 'CSS/UI frameworks',
+      desc:'CSS/UI frameworks to help build great looking websites and applications such as Tailwind CSS, Bootstrap, Materialize, GatsbyJs Bulma and others.',
+      url:'https://letudev.com/cssframeworks',
+    },
+    {
+      id:4,
+      serial:'04',
+      icon:'fa-chart-bar',
+      name: 'Javascript Chart Libraries',
+      desc:'Libraries that help developers visualize data into charts such as ChartJS, Chartist, ApexChartsJs, Google Charts, G2Plot, FrappeJS and others.',
+      url:'https://letudev.com/cssframeworks',
+    },
+    {
+      id:5,
+      serial:'05',
+      icon:'fa-film',
+      name: 'Stock Videos',
+      desc:'List of websites that offer free beautiful stock videos for all kinds of website and apps like Videvo, Pexels, Unsplash, Life of Vids and others. ',
+      url:'https://letudev.com/stockvideos',
+    },
+    {
+      id:6,
+      serial:'06',
+      icon:'fa-hand-scissors',
+      name: 'Online Design',
+      desc:'All kinds of online tools for design, from photo editors to wireframing, and more like Get Waves, Pixlr, Wireframe.cc, RemoveBG, and others.',
+      url:'https://letudev.com/onlinedesigntools',
+    },
+  ]
+
 }
--- src/app/skillup/skillup.component.ts
+++ src/app/skillup/skillup.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./skillup.component.scss']
 })
 export class SkillupComponent {
-
+  links = [
+    {
+      id:1,
+      serial:'01',
+      icon:' fa-house-signal',
+      name: 'Online App',
+      desc:'Start with free programming and coding education app. An interactive way to learn & practice programming online. Improve coding skills to advance in your education and career',
+    },
+    {
+      id:2,
+      serial:'02',
+      icon:'fa-pencil-paintbrush',
+      name: 'Boot camps',
+      desc:'Boot camps are a way to go from novice to expert in just a few months. They cost several thousand dollars or more and require full-time attendance, but there\'s no better way to ramp up quickly.',
+    },
+    {
+      id:3,
+      serial:'03',
+      icon:'fa-fire',
+      name: 'Podcasts',
+      desc:'Sadly, the space here is pretty sparse, but not enough to write it off completely. Only two podcasts are producing regular content and have been around for a while.',
+    },
+    {
+      id:4,
+      serial:'04',
+      icon:'fa-chart-bar',
+      name: 'User Groups',
+      desc:'One of the best ways to learn is to associate with others who are just as passionate. Many developers have demonstrated their commitment to learning by spending their free time to improve themselves.',
+    },
+    {
+      id:5,
+      serial:'05',
+      icon:'fa-film',
+      name: 'People',
+      desc:'There are many personalities in the industry doing a lot of great work. Finding people you admire who are doing work you\'re interested in and following them is a mark of someone interested in learning.',
+    },
+    {
+      id:6,
+      serial:'06',
+      icon:'fa-hand-scissors',
+      name: 'Stack Overflow',
+      desc:'Hands down this is the best place to learn things and get issues solved. In combination with one of the aforementioned online coding tools, you can find a solution for just about any problem here.',
+    },
+  ]
 }
